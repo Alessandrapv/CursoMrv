0ef38c0d525d0e2fba74cc8a7e733d2482934684
Alessandra Paulino <apvprogramacao@gmail.com>
2021-12-02 16:19:03 -0300

    Subindo

---
+++ Contador/assets/js/script.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+const contador = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	contador.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	contador.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
